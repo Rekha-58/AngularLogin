b8a60bbd48620da266f610a3bdc9bfd81188e15f
K. Rekha <rekharadhika2020@gmail.com>
2024-03-27 11:13:51 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
+import { UserComponent } from './user/user.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {ReactiveFormsModule} from '@angular/forms';
+import { MaterialModule } from '../material.module';
+import {HttpClientModule} from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustomerComponent,
+    LoginComponent,
+    RegisterComponent,
+    UpdatepopupComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
+  
   ],
-  providers: [],
+  providers: [
+  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
